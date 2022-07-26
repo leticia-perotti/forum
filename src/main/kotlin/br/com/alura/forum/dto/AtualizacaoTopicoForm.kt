df37ea44ddb9484254bde11e2e695511e247b821
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-07-26 19:15:53 -0300

    Bean validation -> tratamento erros

--- src/main/kotlin/br/com/alura/forum/dto/AtualizacaoTopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/AtualizacaoTopicoForm.kt
@@ -7,10 +7,10 @@ import javax.validation.constraints.Size
 data class AtualizacaoTopicoForm(
     @field:NotNull
     val id: Long,
-    @field:NotEmpty
-    @field:Size(min =5, max =100)
+    @field:NotEmpty(message = "O campo não pode ser nulo")
+    @field:Size(min =5, max =100, message = "Titulo deve ter entre 5 e 100 caracteres")
     val titulo: String,
-    @field:NotEmpty
+    @field:NotEmpty(message = "O campo não pode ser nulo")
     val mensagem: String
 
 ) {
--- src/main/kotlin/br/com/alura/forum/exception/ExeptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exception/ExeptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.exception
 
 import br.com.alura.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -18,6 +19,35 @@ class ExeptionHandler {
         return ErrorView(
             status = HttpStatus.NOT_FOUND.value(),
             error = HttpStatus.NOT_FOUND.name,
+            message = exeption.message,
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(exception: MethodArgumentNotValidException,
+                          request: HttpServletRequest): ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach{
+            e -> errorMessage.put(e.field, e.defaultMessage)
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath
+        )
+    }
+
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleServerError(exeption: Exception,
+                       request: HttpServletRequest): ErrorView {
+        return ErrorView(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             message = exeption.message,
             path = request.servletPath
         )
